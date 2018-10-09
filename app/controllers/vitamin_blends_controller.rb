6dac0b248a582ae9f1757d29f0fae59ea2a8b24d
Przemyslaw Dabrowski <bluh@outlook.com>
2018-10-09 23:40:31 +0000

    created new vitamin page and action

--- app/controllers/vitamin_blends_controller.rb
+++ app/controllers/vitamin_blends_controller.rb
@@ -4,4 +4,19 @@ class VitaminBlendsController < ApplicationController
     @vitamins = VitaminBlend.all
     erb :'vitamin_blends/all'
   end
+
+  get '/vitamins/new' do
+    Helpers.redirect_if_not_logged_in(session)
+    erb :'vitamin_blends/new'
+  end
+
+  post '/vitamins' do
+    Helpers.redirect_if_not_logged_in(session)
+    unless params[:contents] != ""
+      redirect '/vitamin_blends/new'
+    end
+
+    vitamin = VitaminBlend.create(contents: params[:contents], user_id: session[:user_id])
+    redirect '/vitamins'
+  end
 end
